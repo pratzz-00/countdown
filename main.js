44ff000ef51d0e0d3c3c1d3f22c5318a81f41ac0
Pratibha <126044383+pratzz-00@users.noreply.github.com>
2023-06-29 19:33:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
-let launchDate = new Date("Jul 28, 2023 12:00:00").getTime();
+let launchDate = new Date("Jul 29, 2023 12:00:00").getTime();
 
 // Setup Timer to tick every 1 second
 let timer = setInterval(tick, 1000);
